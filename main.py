e7757b24a639d2c86c4cde1b46ca13b21ccf38b9
Smrithiv28 <122669933+Smrithiv28@users.noreply.github.com>
2023-10-19 15:44:24 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,32 @@
+from sklearn.cluster import KMeans
+import matplotlib.pyplot as plt
+import numpy as np
+import cv2
+from skimage.color import rgb2lab, deltaE_cie76
+from collections import Counter
+import os
+
+def RGB2HEX(color):
+    return "#{:02x}{:02x}{:02x}".format(int(color[0]), int(color[1]), int(color[2]))
+
+def get_image(image_path):
+    image = cv2.imread(image_path)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    return image
+
+def get_colors(image,number_of_colors,show_chart):
+    modified_image = cv2.resize(image, (600, 400), interpolation = cv2.INTER_AREA)
+    modified_image = modified_image.reshape(modified_image.shape[0]*modified_image.shape[1], 3)
+    clf = KMeans(n_clusters = number_of_colors)
+    labels = clf.fit_predict(modified_image)
+    counts = Counter(labels)
+    center_colors = clf.cluster_centers_
+    ordered_colors = [center_colors[i] for i in counts.keys()]
+    hex_colors = [RGB2HEX(ordered_colors[i]) for i in counts.keys()]
+    rgb_colors = [ordered_colors[i] for i in counts.keys()]
+    if (show_chart):
+       plt.figure(figsize = (8, 6))
+       plt.pie(counts.values(), labels = hex_colors, colors = hex_colors)
+       return rgb_colors
+    
+get_colors(get_image('Sampleimage.jpeg'),8,True) 
